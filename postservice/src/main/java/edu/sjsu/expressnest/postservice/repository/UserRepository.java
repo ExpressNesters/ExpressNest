2030d0a1763eb37d900aab741baf60619a4f7d63
Neeharika Singh <neeharikasingh@Neeharikas-Air.attlocal.net>
2023-11-20 12:57:22 -0800

    renamed the service

--- postservice/src/main/java/edu/sjsu/expressnest/postservice/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.sjsu.expressnest.postservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.sjsu.expressnest.postservice.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long>{
-
-}

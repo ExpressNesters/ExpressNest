b9a68a621977c9a18b39ebe27127ff0a7e196c0e
Neeharika Singh <neeharikasingh@Neeharikas-Air.attlocal.net>
2023-11-23 22:57:29 -0800

    K8s config

--- feed-service/src/main/java/edu/sjsu/expressnest/feeds/config/KafkaConsumerConfig.java
+++ feed-service/src/main/java/edu/sjsu/expressnest/feeds/config/KafkaConsumerConfig.java
@@ -2,6 +2,7 @@ package edu.sjsu.expressnest.feeds.config;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -15,48 +16,60 @@ import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import edu.sjsu.expressnest.feeds.messaging.FollowEvent;
 import edu.sjsu.expressnest.feeds.messaging.PostEvent;
+import lombok.extern.log4j.Log4j2;
 
 @Configuration
+@Log4j2
 public class KafkaConsumerConfig {
-	
-	@Value("${expressnest.feedservice.kafka.server}")
-	private String bootstrapServer;
-		
-	@Value("${expressnest.feedservice.kafka.group-id}")
-	private String groupId;
-	
-	@Bean
+
+    @Value("${expressnest.feedservice.kafka.server}")
+    private String kafkaServer;
+    
+    @Value("${expressnest.feedservice.kafka.username}")
+    private String kafkaUsername;
+
+    @Value("${expressnest.feedservice.kafka.password}")
+    private String kafkaPassword;
+
+    @Bean
     public ConsumerFactory<String, PostEvent> postEventConsumerFactory() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new JsonDeserializer<>(PostEvent.class));
+        return createConsumerFactory(PostEvent.class);
     }
 
     @Bean
     public ConsumerFactory<String, FollowEvent> followEventConsumerFactory() {
+        return createConsumerFactory(FollowEvent.class);
+    }
+
+    private <T> ConsumerFactory<String, T> createConsumerFactory(Class<T> eventType) {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "feeds-group");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new JsonDeserializer<>(FollowEvent.class));
+        props.put("security.protocol", "SASL_SSL");
+        props.put("sasl.mechanism", "PLAIN");
+        String jaasTemplate = "org.apache.kafka.common.security.plain.PlainLoginModule required username=\'%s\' password=\'%s\';";
+        String jaasCfg = String.format(jaasTemplate, kafkaUsername, kafkaPassword);
+        props.put("sasl.jaas.config", jaasCfg);
+        
+        log.info("props::::::::::::::: {}" , props.values().stream().map(v -> v.toString()).collect(Collectors.joining("\n")));
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new JsonDeserializer<>(eventType));
     }
-    
+
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, PostEvent> postEventKafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, PostEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(postEventConsumerFactory());
-        return factory;
+        return createKafkaListenerContainerFactory(postEventConsumerFactory());
     }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, FollowEvent> followEventKafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, FollowEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(followEventConsumerFactory());
-        return factory;
+        return createKafkaListenerContainerFactory(followEventConsumerFactory());
     }
 
+    private <T> ConcurrentKafkaListenerContainerFactory<String, T> createKafkaListenerContainerFactory(ConsumerFactory<String, T> consumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<String, T> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        return factory;
+    }
 }

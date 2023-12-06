4f82c046864f3ba4b30003d92227819e39bf6ded
Neeharika Singh <neeharikasingh@Neeharikas-Air.attlocal.net>
2023-12-05 20:12:27 -0800

    sslmode default

--- user_profile_management/main.go
+++ user_profile_management/main.go
@@ -63,7 +63,7 @@ func corsMiddleware(next http.Handler) http.Handler {
 func main() {
 	// Initialize the database connection
 	db, err := sql.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s "+
-		"password=%s dbname=%s sslmode=disable",
+		"password=%s dbname=%s",
 		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"),
 		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME")))
